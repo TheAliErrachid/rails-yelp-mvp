7548cd39a537f5c77f9f5b59a267552cab79288d
Ali Errachid <errachid.ali.perso@gmail.com>
2023-08-16 16:12:58 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+Restaurant.create([
+  {name: "Dominos", address: "16 villa gaudelet, Paris 11", category: "french"},
+  {name: "Pepino", address: "13 villa gaudelet, Paris 11", category: "french"},
+  {name: "Mazard", address: "36 villa gaudelet, Paris 11", category: "french"},
+  {name: "Pomodoro", address: "56 villa gaudelet, Paris 11", category: "french"},
+  {name: "Zebra", address: "76 Svilla gaudelet, Paris 11", category: "french"}
+])
+
+# [dominos, pepino, mazard, pomodoro, zebra].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+puts "Finished!"
